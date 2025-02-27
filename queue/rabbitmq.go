31fafb6791681971b7e3718ba17a60d01824394e
Xu Feiyang <3316560771@qq.com>
2025-02-27 14:53:54 +0800

    feat: MQ add x-retry

--- queue/rabbitmq.go
+++ queue/rabbitmq.go
@@ -124,6 +124,7 @@ func SendMsg(queueName string, msg string) error {
 	return ch.Publish("", queueName, false, false, amqp091.Publishing{
 		ContentType: "text/plain",
 		Body:        []byte(msg),
+		Headers:     amqp091.Table{"x-retry": 0}, // 初始化重试次数为0
 	})
 }
 
